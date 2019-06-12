98df23d92fb62dafb0c6bf31c1ef03fd93671aa3
Asad Hasan <asadhasan180@hotmail.com>
2019-06-12 17:20:52 +0100

    initial commit

---
+++ cypress/integration/BulkLookupPostcodesSpec.ts
@@ -0,0 +1,25 @@
+describe("Bulk lookup of postcodes", () => {
+  it("should return correct results", () => {
+    const method = "POST";
+    const body = {
+      postcodes: ["OX49 5NU", "M32 0JG", "NE30 1DP"]
+    };
+
+    cy.request(method, "postcodes", body).then(response => {
+      const responseBody = response.body.result;
+
+      expect(responseBody[0].result).to.have.property(
+        "parish",
+        "Brightwell Baldwin"
+      );
+      expect(responseBody[1].result).to.have.property(
+        "parish",
+        "Trafford, unparished area"
+      );
+      expect(responseBody[2].result).to.have.property(
+        "parish",
+        "North Tyneside, unparished area"
+      );
+    });
+  });
+});
